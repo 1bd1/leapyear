1aa4ee4c64a7998c207e688b3092ec9640374f30
Dibran <190604113+1bd1@users.noreply.github.com>
2025-01-27 20:51:03 +0100

    4th version of Leapyear Mon Jan 27 20:51:02 CEST 2025

--- src/leapyear.java
+++ src/leapyear.java
@@ -1,36 +1,33 @@
-public class leapyear {
-    public static void main(String[] args) {
+public class leapyear
+{
+    public static void main(String[] args)
 
+    {
         int rok = 2000;
 
-// Testujem centenialne roky 1X00, 2X00
-
-        String rokstring = Integer.toString(rok);
-        String cent_rok = (rokstring.substring(2, 4));
-        int centRokInt = Integer.parseInt(cent_rok);
-        //System.out.println(centRokInt);
-
-        if (centRokInt ==0) {
-            if (rok % 400 == 0) {
-                System.out.print(rok);
-                System.out.println(" je priestupny rok [div400]");
-                return;
-            }
-
+        if ( rok % 400 == 0)
+        {
             System.out.print(rok);
-            System.out.println(" je nepriestupny rok [non-div-400]");
-            return;
+            System.out.println(" je priestupny rok");
 
         }
+        else if (rok % 100 == 0)
+            {
+                    System.out.print(rok);
+        System.out.println(" je nepriestupny rok");
+        }
 
-// Testujem vsetky ostatne roky ak nepresli centenialnym testom
+        else if (rok % 4 == 0)
+        {
+            System.out.print(rok);
+            System.out.println(" je priestupny rok");
+        }
 
-        if (rok % 4 == 0) {
+        else
+
+        {
             System.out.print(rok);
-            System.out.println(" je priestupny rok [div4]");
-        } else {
-            System.out.print(rok);
-            System.out.println(" je nepriestupny rok [non-div-4]");
+            System.out.println(" je nepriestupny rok");
         }
 
     }
